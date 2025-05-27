6dd0c616fecd808f52877193537e73d16c682106
Isaiah Paget <isaiahpaget@gmail.com>
2025-05-26 22:25:46 -0700

    file paths

--- src/main.c
+++ src/main.c
@@ -7,10 +7,30 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-struct msgstruct {
+#define BUFFER_SIZE 4096
+#define METHOD_BUFFER_SIZE 8
+#define PATH_BUFFER_SIZE 1024
+#define VERSION_BUFFER_SIZE 16
+
+typedef struct Response {
         uint32_t length;
-        char send_data[4096];
-};
+        char send_data[BUFFER_SIZE];
+} Response;
+
+typedef struct Request {
+        uint32_t length;
+        char receive_data[BUFFER_SIZE];
+		char method[METHOD_BUFFER_SIZE];
+		char path[PATH_BUFFER_SIZE];
+		char version[VERSION_BUFFER_SIZE];
+} Request;
+
+void parse_request(Request *request) {
+	printf("%s\n", request->receive_data);
+	printf("%d\n", request->length);
+
+	sscanf(request->receive_data, "%s %s %s\r\n", request->method, request->path, request->version);
+}
 
 int main() {
 	// Disable output buffering
@@ -59,11 +79,28 @@ int main() {
 	client_addr_len = sizeof(client_addr);
 
 
-	struct msgstruct response;
-	strncpy(response.send_data, "HTTP/1.1 200 OK\r\n\r\n", sizeof(response.send_data));
 	int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+
+	Request request;
+	int request_length = read(client_fd, request.receive_data, 128);
+	if (request_length < 0) {
+		printf("\nSocket read error.");
+		exit(1);
+	}
+	request.length = request_length;
+
+	parse_request(&request);
+
+	Response response;
+
+	if (strcmp(request.path, "/") == 0) {
+		strncpy(response.send_data, "HTTP/1.1 200 OK\r\n\r\n", sizeof(response.send_data));
+	} else {
+		strncpy(response.send_data, "HTTP/1.1 404 Not Found\r\n\r\n", sizeof(response.send_data));
+	}
+
 	int error = send(client_fd, response.send_data, strlen(response.send_data), 0);
-	if (error == -1) {
+	if (error < 0) {
 		printf("\nSocket error.");
 		exit(1);
 	}
